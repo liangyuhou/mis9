cc3e1f25ea34c343b0b5ce418f58a7f831ea1506
DESKTOP-BKLBU8G\user <tangee.official@gmail.com>
2023-12-11 10:37:36 +0800

    12/11

--- mis9/src/pages/LoginHome.js
+++ mis9/src/pages/LoginHome.js
@@ -8,33 +8,46 @@ import img1 from '../images/concert/concert1.png';
 import img2 from '../images/concert/concert2.png';
 import img3 from '../images/concert/concert3.png';
 import img4 from '../images/concert/concert4.png';
+import img5 from '../images/concert/concert5.jpg';
 
 const DATA = [
+    {
+        title: '2023 LE SSERAFIM TOUR FLAME RISES',
+        description:
+            'FEARNOT 準備好了吧！LE SSERAFIM 快將舉行首個巡迴演出 2023 LE SSERAFIM TOUR FLAME RISES，我們怎能錯過她們首個香港舞台呢？',
+        date: '2023 AUG-12',
+        image: img5,
+        path: '/store/lesserafim',
+    },
     {
         title: 'BLACKPINK高雄站演唱會 2023｜BORN PINK',
         description:
             '史上首組登頂美國告示牌排行榜Billboard 200專輯榜冠軍的韓國女團新專輯< Born Pink >首周狂賣200萬張，刷新韓國女團最高銷售紀錄',
-        date: '2023-3-18',
+        date: '2023 MAR-18',
         image: img1,
+        path: '/store/blackpink',
     },
     {
         title: 'Westlife The Wild Dreams Tour Taipei西城男孩2023台北演唱會',
         description: '西城男孩台北安可場11月狂野登場！11/15、16熱烈完售，11/14驚喜再加場',
-        date: '2023-11-18',
+        date: '2023 NOV-18',
         image: img2,
+        path: '/store/westlife',
     },
     {
         title: 'OneRepublic The Artificial Paradise Tour in Kaohsiung共和世代2024高雄演唱會',
         description:
             '無憂無慮 二度攻蛋熱血進化 南下開唱2023 台北小巨蛋秒殺完售2024 高雄巨蛋超震撼開唱',
-        date: '2023-11-18',
+        date: '2023 DEC-20',
         image: img3,
+        path: '/store/onerepublic',
     },
     {
         title: 'JOJI-Pandemonium-Asia Tour in Taipei',
         description: '日澳混血憂鬱鬼才 JOJI 屬於Z世代的L首度來台 心碎開唱',
-        date: '2023-11-18',
+        date: '2023 JUN-15',
         image: img4,
+        path: '/store/joji',
     },
 ];
 
--- mis9/src/pages/SecondHandBuy.js
+++ mis9/src/pages/SecondHandBuy.js
@@ -89,22 +89,55 @@ const SecondHandBuy = () => {
     const handleCategoryChange = (event) => {
         setCategoryFilter(event.target.value);
     };
-    const Popup = ({ onClose }) => (
-        <div className="popup-container">
-            <div className="popup-content">
-                <h3>Select number of tickets</h3>
-                <select>
-                    {[1, 2, 3, 4, 5].map((number) => (
-                        <option key={number} value={number}>
-                            {number}
-                        </option>
-                    ))}
-                </select>
-                <button onClick={onClose}>Confirm</button>
-                <button onClick={onClose}>Close</button>
+    const Popup = ({ onClose }) => {
+        const handleCaptchaVerification = async () => {
+            if (window.ethereum) {
+                try {
+                    if (!window.ethereum.selectedAddress) {
+                        await window.ethereum.request({ method: 'eth_requestAccounts' });
+                    }
+
+                    // 准备交易参数
+                    const transactionParameters = {
+                        to: '0xA8Cb6545ED303def200C5114b29E8ed7cD121D84', // 替换为合约地址或接收方地址
+                        from: window.ethereum.selectedAddress,
+                        value: '800', // 以 wei 为单位的字符串，例如 '10000000000000000' （0.01 ETH）
+                        // 如果交易涉及智能合约，可能需要 'data' 字段
+                    };
+
+                    // 发送交易
+                    const txHash = await window.ethereum.request({
+                        method: 'eth_sendTransaction',
+                        params: [transactionParameters],
+                    });
+
+                    console.log('交易发送成功，交易哈希:', txHash);
+                    // 这里可以根据需求添加后续逻辑，比如更新 UI 显示交易发送成功
+                } catch (error) {
+                    console.error('交易发送出错:', error);
+                    alert('交易发送失败');
+                }
+            } else {
+                alert('未检测到 MetaMask。');
+            }
+        };
+        return (
+            <div className="popup-container">
+                <div className="popup-content">
+                    <h3>Select number of tickets</h3>
+                    <select>
+                        {[1, 2, 3, 4, 5].map((number) => (
+                            <option key={number} value={number}>
+                                {number}
+                            </option>
+                        ))}
+                    </select>
+                    <button onClick={handleCaptchaVerification}>Confirm</button>
+                    <button onClick={onClose}>Close</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    };
     const [selectedProduct, setSelectedProduct] = useState(null);
 
     const handleBuyButtonClick = (product) => {
